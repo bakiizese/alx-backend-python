b9dc19f17c7cff7b89e9006c78223d14ca830aba
root <bereketzeselassie@gmail.com>
2024-04-18 14:02:34 +0000

    annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import math
+'''floor annotated'''
+
+
+def floor(n: float) -> int:
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''to_str annotated'''
+
+
+def to_str(n: float) -> str:
+    return str(n)
