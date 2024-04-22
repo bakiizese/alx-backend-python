8c4d52a7d676745f0ffb48d4e431194323abc15e
root <bereketzeselassie@gmail.com>
2024-04-22 20:35:20 +0000

    tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''multi coroutines'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''multi'''
+    coroutines: List = [wait_random(max_delay) for _ in range(n)]
+    rlist: List[float] = await asyncio.gather(*coroutines)
+
+    return sorted(rlist)
