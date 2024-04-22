5f71d7c3df9ece1cc7f6ee84f6c296fa3d5dfbf4
root <bereketzeselassie@gmail.com>
2024-04-22 13:40:27 +0000

    fix;

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ from typing import List
 
 
 async def wait_n(n: int, max_delay: int) -> float:
+    '''multi'''
     wait_random = __import__('0-basic_async_syntax').wait_random
     coroutines: List = [wait_random(max_delay) for _ in range(n)]
     rlist: List[float] = await asyncio.gather(*coroutines)
